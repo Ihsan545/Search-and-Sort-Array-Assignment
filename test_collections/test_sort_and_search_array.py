fa9a51a26cb4601571010884ffd070501a4868ae
anwar <Ihsan545>
2020-10-12 20:27:37 -0500

    Test passed

--- test_collections/test_sort_and_search_array.py
+++ test_collections/test_sort_and_search_array.py
@@ -5,7 +5,7 @@ import fun_with_collections.sort_and_search_array as basic_list_exception
 
 class MyTestCase(unittest.TestCase):
     def test_search_array(self):
-        self.assertFalse(basic_list_exception.search_array([], -1))
+        self.assertFalse(basic_list_exception.search_array([5, -1, 3, 6, 8, 9, 4, 10], -1))
 
 
 def test_sort_array(self):
@@ -14,7 +14,7 @@ def test_sort_array(self):
     _list = to_list.sort()
     to_list = r.array(to_list)
 
-    self.assertTrue(basic_list_exception.sort_array(([])))
+    self.assertTrue(basic_list_exception.sort_array(([9.0, 3.0, 1.0, 5.0, 8.0, 6.0, 4.0, 7.0])))
     """" I tried my bet, but I could not figure out the sort test"""
 
 
